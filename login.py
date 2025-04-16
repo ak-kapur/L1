3d89286e56ee47238def87aaf46a6f9b8357eedd
ak-kapur <116184741+ak-kapur@users.noreply.github.com>
2025-04-16 11:24:42 +0000

    da

--- login.py
+++ login.py
@@ -7,6 +7,6 @@ st.write("Select your role to proceed:")
 role = st.selectbox("Choose Role", ["Select", "Judge", "Lawyer"])
 
 if role == "Judge":
-    st.switch_page("app6(Judge).py")
+    st.switch_page("app7(Final).py")
 elif role == "Lawyer":
     st.switch_page("trial.py")
